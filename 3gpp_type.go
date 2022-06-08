73aac4c830102e5754c4081c1fa1d4461af1f4f6
Ajay Lotan Thakur <ajay.thakur@intel.com>
2022-06-07 18:01:39 -0600

    Updating license information

--- 3gpp_type.go
+++ 3gpp_type.go
@@ -1,3 +1,6 @@
+// Copyright 2019 Communication Service/Software Laboratory, National Chiao Tung University (free5gc.org)
+//
+// SPDX-License-Identifier: Apache-2.0
 package util_3gpp
 
 type Dnn []uint8
